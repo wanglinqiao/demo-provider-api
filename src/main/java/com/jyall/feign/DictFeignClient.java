7165ea52539a86aa2a105c7c1c9d0ac6c588a716
wanglinqiao <wang.linqiao@jyall.com>
2016-11-03 19:11:19 +0800

    init F-001

--- src/main/java/com/jyall/feign/DictFeignClient.java
+++ src/main/java/com/jyall/feign/DictFeignClient.java
@@ -1,11 +1,8 @@
 package com.jyall.feign;
 
-import com.jyall.pojo.Dict;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
 
 /**
  * Created by wang.linqiao on 2016/11/2.
